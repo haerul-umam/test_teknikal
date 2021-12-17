aecee125a656cd917363bd09990548a90f91bd8a
umamilano <dummytgr2@gmail.com>
2021-12-17 16:56:39 +0700

    create home and show data in card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-
+import Info from "./components/card/Info";
+import {API} from "./config/api"
+import {useState, useEffect} from "react"
 
 function App() {
+  const [data, setData] = useState([])
+
+  const getAllData = async () => {
+    try {
+        const response = await API.get("/posts")
+        setData(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getAllData()
+  //eslint-disable-next-line
+  },[])
+
   return (
     <div className="App">
-   
+      <div className="d-flex flex-wrap">
+        {
+          data.map((item) => {
+            return <Info key={item.id} data={item}/>
+          })
+        }
+      </div>
     </div>
   );
 }
--- src/components/card/Info.jsx
+++ src/components/card/Info.jsx
@@ -1,18 +1,19 @@
-import {Card, Button} from "react-bootstrap"
+import {Card} from "react-bootstrap"
 import style from "./Info.module.css"
 import {Link} from "react-router-dom"
 
 export default function Info({data}) {
+    console.log(data)
     return (
-        <Card>
+        <Card className={style.Card}>
             <Card.Body>
                 <Card.Title>
+                    <p>test</p>
                     {data.title}
                 </Card.Title>
                 <Card.Text>
                     {data.Body}
                 </Card.Text>
-                <Button variant="primary">Detail</Button>
             </Card.Body>
         </Card>
     )
